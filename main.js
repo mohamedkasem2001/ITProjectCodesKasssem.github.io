5da92d6d668b00c9ac57799243fa729a2ef6438d
mohamedkasem2001 <85518259+mohamedkasem2001@users.noreply.github.com>
2021-06-07 21:58:45 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	
+	
+	$(".sign-up").animate({
+		
+		width:"70",
+		height:"70",
+		top:"-700px",
+		left:"47%",
+		borderRadius:"100%",
+		
+				
+	},2000);
+	
+	$(".sign-up").animate({
+		
+		width:"100",
+		height:"100",
+		top:"200px",
+		left:"47%",
+		
+		
+		
+				
+	},2500);
+	
+	$(".sign-up").animate({
+		
+		width:"250px",
+		height:"350px",
+		top:"50px",
+		left:"40%",
+		borderRadius:"1%",
+		
+				
+	},500);
+	
+});
